a410ef019c489be39325b0373086d97d5df75752
Quentin Santos <qsantos@qsantos.fr>
2024-08-24 17:39:05 +0200

    Display global success rate

--- main.py
+++ main.py
@@ -11,3 +11,10 @@ sessions = pd.DataFrame(data["sessions"])
 
 print(characters)
 print(sessions)
+print()
+
+correct = (characters.result == "Correct").sum()
+total = len(characters.index)
+success_rate = correct / total
+
+print(f"Global success rate: {success_rate * 100:.1f}%")
