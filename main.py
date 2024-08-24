f1d8c3237eb31c56bc4685c46dfcc83fc367b7a4
Quentin Santos <qsantos@qsantos.fr>
2024-08-24 23:08:53 +0200

    Simplify code for rolling window success rate

--- main.py
+++ main.py
@@ -21,10 +21,6 @@ success_rate = correct / total
 print(f"Global success rate: {success_rate * 100:.1f}%")
 
 characters["correct"] = characters.result == "Correct"
-w = characters.rolling(window=10000)
-correct = w.correct.sum()
-total = 10000
-success_rate = correct / total
-success_rate.plot()
+characters.rolling(window=10000).correct.mean().plot()
 
 plt.show(block=True)
